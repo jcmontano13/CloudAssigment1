f211ca815fd5b9c23f320da2b47880d515b8894b
Aeriel Matthew Cruz <aerielmatthew@gmail.com>
2025-08-11 14:43:06 +1200

    Remove comments

--- data/database.js
+++ data/database.js
@@ -7,10 +7,7 @@ const MongoClient = mongodb.MongoClient;
 let database;
 
 async function connecToDatabse() {
-    // const client = await MongoClient.connect('mongodb://localhost:27017'); // default port
-    // const MONGODB_URI = process.env.MONGODB_URI;
     const MONGODB_URI = 'mongodb+srv://jeckmontano:cloudApplicationAIS@cloudapplicationais.ftzcy2s.mongodb.net/?retryWrites=true&w=majority&appName=CloudApplicationAIS'
-    // const client = await MongoClient.connect('mongodb+srv://jeckmontano:aiscloudapplication@cloudapplicationais.64i13zm.mongodb.net/?retryWrites=true&w=majority&appName=CloudApplicationAIS'); // default port
     const client = await MongoClient.connect(MONGODB_URI); // default port
     database = client.db('online-shop');
 }
